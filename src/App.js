9390f57ab730336c4d6f049f9cb62a78491fa49e
bobpruz <bobpruz@gmail.com>
2022-06-18 06:50:02 -0500

    added work content backed up App.css

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Bio from './components/Bio'
 import Contact from './components/Contact';
 import Header from './components/Header';
+import Work from './components/Work';
 
 function App() {
   return (
     <>
     <Header />
     <Bio />
+    <Work />
     <Contact />
     </>
     
---
+++ src/components/Work/index.js
@@ -0,0 +1,82 @@
+import React from "react";
+
+function Work() {
+    return (
+        <section id="work" class="work">
+        <div class="section-name">
+          <h3>My Work</h3>
+        </div>
+        <div class="work-grid">
+  
+          <div class="work1">
+            <div class="work2">
+              <div class="language-big">
+                 <h4>Run Buddy</h4>
+                 <p>HTML/CSS</p>
+                </div>
+              <a href="https://bobpruz.github.io/runbuddy/index.html">
+                <img
+                  src="./assets/images/work1.jpg"
+                  alt="Run Buddy Website Screenshot"
+                />
+              </a>
+            </div>
+          </div>
+  
+  
+          <div class="work-card">
+            <a href="https://bobpruz.github.io/urban/Develop/index.html">
+              <div class="work-cards">
+                <div class="language-small">
+                  <h4>Horiseon</h4>
+                 <p>HTML/CSS</p>
+                </div>
+                <img
+                  src="./assets/images/horiseon.jpg"
+                  alt="Horiseon Website Screenshot"
+                />
+              </div>
+            </a>
+            <a href="https://bobpruz.github.io/project1/index.html">
+              <div class="work-cards">
+                <div class="language-small">
+                  <h4>GYDSR</h4>
+                 <p>HTML/CSS/JS/JQUERY/APIs</p>
+                </div>
+                <img
+                  src="./assets/images/project1.jpg"
+                  alt="Get Your Day Started Right Website Screenshot"
+                />
+              </div>
+            </a>
+            <a href="https://bobpruz.github.io/runbuddy/index.html">
+              <div class="work-cards">
+                <div class="language-small">
+                  <h4>Run Buddy</h4>
+                 <p>HTML/CSS</p>
+                </div>
+                <img
+                  src="./assets/images/work1.jpg"
+                  alt="Run Buddy Website Screenshot"
+                />
+              </div>
+            </a>
+            <a href="https://bobpruz.github.io/runbuddy/index.html">
+              <div class="work-cards">
+                <div class="language-small">
+                  <h4>Run Buddy</h4>
+                 <p>HTML/CSS</p>
+                </div>
+                <img
+                  src="./assets/images/work1.jpg"
+                  alt="Run Buddy Website Screenshot"
+                />
+              </div>
+            </a>
+          </div>
+        </div>
+      </section>
+    )
+}
+
+export default Work;
